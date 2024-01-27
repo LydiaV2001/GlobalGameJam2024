7da8fabf69e1c65fb87f65da82373fbdd90fe183
MmmGarlicFingers <lpunyu13@gmail.com>
2024-01-27 17:17:46 -0400

    Added sex

---
+++ scripts/constants/randomData.js
@@ -0,0 +1,165 @@
+export const randomSqueaks = {
+    users: [
+        {
+            author: "Mickey Moose",
+            tag: "@RealMickeyMouse",
+            pfp: "./images/mickey.jpg",
+        },
+        {
+            author: "Micheal Mouse",
+            tag: "@MichealMouse_11",
+            pfp: "./images/mickey.jpg",
+        },
+        {
+            author: "Miccy Mouse",
+            tag: "@MickeMousOfficia",
+            pfp: "./images/mickey.jpg",
+        },
+        {
+            author: "Walter White",
+            tag: "@Heisenberg",
+            pfp: "./images/walter.jpg"
+        }
+    ],
+    squeaks: [ 
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "So tired..",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Mickey Mouse is so cool!",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Mickey Mouse is the bomb!",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "I sure do hope that Mickey Mouse doesn't have any weird things in his past!",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Hope nothing surfaces about our old friend mickey mouse..",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Eating food just to stay alive...",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Can't see the sun for the stars..",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "I sure do love this new Mickey Mouse Movie.",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Can't eat without even thinking.",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Spaghetti!",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Just another brick in the wall.",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "I can't even believe what this new mickey tweet said!",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "I weep for the wheat",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "I love the new song 'gangnam style'",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Little bit sleepy",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Little bit sleepy",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "little bit tired.",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Nothing about this is good",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "You should be ashamed of yourself",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Turn back",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Keep the waters clean",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "Preach the horror",
+            image: ""
+        },
+        {
+            date: "Today",
+            category: "nonMickey",
+            body: "I sure do love eating cheese!",
+            image: ""
+        }
+    ]
+    
+}
--- scripts/squeakFactory.js
+++ scripts/squeakFactory.js
@@ -1,8 +1,61 @@
 import { squeakData } from "./constants/squeakData.js";
+import { randomSqueaks } from "./constants/randomData.js";
+
+const compareDates = (tweet1, tweet2) => {
+    const date1 = new Date(tweet1.date);
+    const date2 = new Date(tweet2.date);
+    return date2 - date1;
+};
+
+function getRandomItem(list) {
+  const randomIndex = Math.floor(Math.random() * list.length);
+  return list[randomIndex];
+};
+
+function getRandomDate() {
+  const startDate = new Date('01/01/2009');
+  const endDate = new Date('12/25/2023');
+  const startTimestamp = startDate.getTime();
+  const endTimestamp = endDate.getTime();
+  
+  const randomTimestamp = startTimestamp + Math.random() * (endTimestamp - startTimestamp);
+  const randomDate = new Date(randomTimestamp);
+  const mm = String(randomDate.getMonth() + 1).padStart(2, '0'); // January is 0!
+  const dd = String(randomDate.getDate()).padStart(2, '0');
+  const yy = String(randomDate.getFullYear()).slice(-2);
+
+  return `${mm}/${dd}/${yy}`;
+}
+
+
+
+function addRandomSqueaks(data) {
+    let author = getRandomItem(randomSqueaks.users);
+    let squeak = getRandomItem(randomSqueaks.squeaks);
+    let newData = {
+        author: author.author,
+        tag: author.tag,
+        date: getRandomDate(),
+        pfp: author.pfp,
+        category: squeak.category,
+        body: squeak.body,
+        image: "",
+        likes: Math.floor(Math.random() * 100),
+        resqueaks: Math.floor(Math.random() * 100),
+        replies: Math.floor(Math.random() * 100),
+    }
+    data.push(newData);
+}
 
 export function buildSqueaks() {
 
     let data = squeakData.squeaks;
+
+    for (let i = 0; i < 10; i++) {
+        addRandomSqueaks(data)
+    }
+
+    data.sort(compareDates)
     
     for (let i = 0; i < data.length; i++) {
     
@@ -36,7 +89,7 @@ export function buildSqueaks() {
         let text = document.createElement("p");
         text.classList.add("squeakText");
         text.innerHTML = data[i].body;
-    
+        
         let image = document.createElement("img");
         image.classList.add("squeakImage");
         image.setAttribute("src", data[i].image);
@@ -75,7 +128,11 @@ export function buildSqueaks() {
     
         body.appendChild(userInfo);
         body.appendChild(text);
-        body.appendChild(image);
+        if (data[i].image != "") {
+            body.appendChild(image);
+        } else {
+            console.log("Fuck")
+        }
         body.appendChild(engagement);
     
         engagement.appendChild(reply);
