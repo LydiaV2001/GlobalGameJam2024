4d3a079c13a2676c067c4c9e5666723c5e25658d
MmmGarlicFingers <lpunyu13@gmail.com>
2024-01-28 16:08:26 -0400

    Changed timer

--- scripts/timer.js
+++ scripts/timer.js
@@ -15,7 +15,7 @@ const endSqueak = {
     }
 
 export function timer() {
-    var sec = 1;
+    var sec = 150;
     var timer = setInterval(function () {
         let min = Math.floor(sec/60)
         if (sec%60 < 10) {
