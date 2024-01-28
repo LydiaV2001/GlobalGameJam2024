b56243a1a68054dedbcc903bf7a87ec576b377a4
lydia <parker.verboom@gmail.com>
2024-01-28 16:18:55 -0400

    no music

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 import { buildSqueaks } from "./squeakFactory.js";
 import { buildObjectives, calculatePoints } from "./writepad.js";
 import { timer } from "./timer.js";
-const audioElement = new Audio("../Assets/audio/Mickeyyyyyy.mp3");
+//const audioElement = new Audio("../Assets/audio/Mickeyyyyyy.mp3");
 
 function startGame() {
-	audioElement.play();
-	audioElement.loop(true);
+	//audioElement.play();
+	//audioElement.loop(true);
 	buildSqueaks();
 	buildObjectives();
 	timer();
