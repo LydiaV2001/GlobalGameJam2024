05c121cb7a6dd645a9660802b513106047c9bcff
MmmGarlicFingers <lpunyu13@gmail.com>
2024-01-28 15:31:07 -0400

    Simmple loop

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import { buildSqueaks } from "./squeakFactory.js";
 import { buildObjectives, calculatePoints } from "./writepad.js";
 import { timer } from "./timer.js";
 
+<<<<<<< Updated upstream
 buildSqueaks();
 buildObjectives();
 timer();
@@ -10,3 +11,12 @@ timer();
 const audioElement = new Audio("../Assets/audio/Mickeyyyyyy.mp3");
 audioElement.play();
 audioElement.loop(true);
+=======
+function startGame() {
+	buildSqueaks();
+	buildObjectives();
+	timer();
+}
+
+startGame();
+>>>>>>> Stashed changes
