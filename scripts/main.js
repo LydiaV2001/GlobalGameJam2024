92b86a28c4237f9f6b7022598b63131cba6dc1d8
MmmGarlicFingers <lpunyu13@gmail.com>
2024-01-28 15:33:14 -0400

    Fixed (ignore last commit pls pls pls)

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,16 @@
 import { buildSqueaks } from "./squeakFactory.js";
 import { buildObjectives, calculatePoints } from "./writepad.js";
 import { timer } from "./timer.js";
+//const audioElement = new Audio("../Assets/audio/Mickeyyyyyy.mp3");
 
-<<<<<<< Updated upstream
-buildSqueaks();
-buildObjectives();
-timer();
-//calculatePoints();
 
-const audioElement = new Audio("../Assets/audio/Mickeyyyyyy.mp3");
-audioElement.play();
-audioElement.loop(true);
-=======
 function startGame() {
+	//audioElement.play();
+	//audioElement.loop(true);
 	buildSqueaks();
 	buildObjectives();
 	timer();
 }
 
 startGame();
->>>>>>> Stashed changes
+
